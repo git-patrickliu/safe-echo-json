6e86855ff668b636f4140a92c65ef895de20a4fd
zhangshanxiang <zhangshanxiang@f-road.com.cn>
2017-06-14 17:24:24 +0800

    对obj的key也加入转义，防止出现在key中的XSS攻击

--- index.js
+++ index.js
@@ -1,14 +1,10 @@
 /**
  * Created by amos on 14-5-12.
+ * --------------------------
+ * Update:
+ * 2017-06-14: by brysonzhang 
+ * 对obj的key也加入转义，防止出现在key中的XSS攻击
  */
 module.exports = function(obj){
-    return JSON.stringify(obj, replacer).replace(/\\u0000\\u0001/g, '\\');
-};
-
-function replacer(key, value){
-    var RE = /[& <>'"\/]/;
-
-    return typeof value === 'string' && RE.test(value) ?
-            value.replace(/([& <>'\/])/g, '\x00\x01$1')
-            : value;
+    return JSON.stringify(obj).replace(/([& <>'\/])/g, '\\$1');
 };
